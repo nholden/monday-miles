e6ccbd0046453490fb0d4dfae0a3b8aa1ce309fe
Nick Holden <nick@realhq.com>
2017-10-09 15:23:01 -0700

    Vendor connect with Strava image assets

--- config/application.rb
+++ config/application.rb
@@ -27,5 +27,7 @@ module MondayMiles
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'strava', 'btn_strava_connectwith_orange')
   end
 end
