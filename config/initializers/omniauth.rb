07da4d388c80d83af97e90861093ac23ed5d5502
Nick Holden <nick@realhq.com>
2017-10-12 18:26:30 -0700

    Rename environment variable for clarity

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :strava, ENV.fetch('STRAVA_CLIENT_ID'), ENV.fetch('STRAVA_API_KEY'), scope: 'public'
+  provider :strava, ENV.fetch('STRAVA_CLIENT_ID'), ENV.fetch('STRAVA_CLIENT_SECRET'), scope: 'public'
 end
