9a37c9dac8a4425e8bc56c7f4c89ea16e07caa08
Nick Holden <nick.r.holden@gmail.com>
2017-11-22 11:21:47 -0800

    Use dynamic attributes in factories to ensure they use Time defined in specs

--- spec/factories/activity_factories.rb
+++ spec/factories/activity_factories.rb
@@ -2,22 +2,22 @@ FactoryGirl.define do
   factory :activity do
 
     trait :last_monday do
-      start_time Time.current.monday
+      start_time { Time.current.monday }
       monday true
     end
 
     trait :two_mondays_ago do
-      start_time Time.current.monday - 1.week
+      start_time { Time.current.monday - 1.week }
       monday true
     end
 
     trait :four_mondays_ago do
-      start_time Time.current.monday - 3.weeks
+      start_time { Time.current.monday - 3.weeks }
       monday true
     end
 
     trait :wednesday do
-      start_time Time.current.monday + 2.days
+      start_time { Time.current.monday + 2.days }
       monday false
     end
 
