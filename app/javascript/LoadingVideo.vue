c06b0c3685cb8313a246643a25ee8d06511f6372
Nick Holden <nick.r.holden@gmail.com>
2017-12-16 00:09:17 -0800

    Move loading video into Vue component

---
+++ app/javascript/LoadingVideo.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+  #player
+</template>
+
+<script lang="coffee">
+import YouTubePlayer from 'youtube-player'
+
+export default
+  props:
+    redirectPath:
+      required: true
+
+  mounted: ->
+    @mountPlayer()
+    setTimeout(@redirect, 60000)
+
+  methods:
+    mountPlayer: -> YouTubePlayer('player',
+      height: '390'
+      width: '640'
+      videoId: '98mBl9d0dew'
+      playerVars:
+        'autoplay': 1
+        'controls': 0
+        'disablekb': 1
+        'modestbranding': 1
+        'showinfo': 0
+      ).on('stateChange', (event) =>
+        @redirect() if (event.data == 0)
+      )
+    redirect: -> window.location = @redirectPath
+</script>
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,12 +1,13 @@
-import TurbolinksAdapter from 'vue-turbolinks';
+import TurbolinksAdapter from 'vue-turbolinks'
 import Vue from 'vue/dist/vue.esm'
 import Activities from '../Activities.vue'
+import LoadingVideo from '../LoadingVideo.vue'
 
 Vue.use(TurbolinksAdapter)
 
 document.addEventListener('turbolinks:load', () => {
   const app = new Vue({
     el: '#vue-app',
-    components: { Activities }
+    components: { Activities, LoadingVideo }
   })
 })
