3b01c6a6b8a9ab36812d5427a5ec4d80a00b5621
Nick Holden <nick.r.holden@gmail.com>
2017-12-31 06:41:13 -0800

    Remove year-to-date stats from profile head (now shown with activities)

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -19,48 +19,4 @@ class UserDecorator < Draper::Decorator
     object.monday_activities.pluck(:start_time).map(&:year).uniq.to_json
   end
 
-  def ytd_monday_miles
-    monday_miles_in_year(Time.current.year)
-  end
-
-  def ytd_monday_feet_elevation_gain
-    monday_feet_elevation_gain_in_year(Time.current.year)
-  end
-
-  def ytd_monday_hours
-    monday_hours_in_year(Time.current.year)
-  end
-
-  def ytd_monday_activities_count
-    monday_activities_count_in_year(Time.current.year)
-  end
-
-  def monday_miles_in_year(year)
-    h.number_with_precision(
-      Meters.new(object.monday_activities.in_year(year).pluck(:distance).reduce(&:+)).to_miles,
-      precision: 1,
-      delimiter: ','
-    )
-  end
-
-  def monday_feet_elevation_gain_in_year(year)
-    h.number_with_precision(
-      Meters.new(object.monday_activities.in_year(year).pluck(:total_elevation_gain).reduce(&:+)).to_feet,
-      precision: 0,
-      delimiter: ','
-    )
-  end
-
-  def monday_hours_in_year(year)
-    h.number_with_precision(
-      Seconds.new(object.monday_activities.in_year(year).pluck(:moving_time).reduce(&:+)).to_hours,
-      precision: 1,
-      delimiter: ','
-    )
-  end
-
-  def monday_activities_count_in_year(year)
-    object.monday_activities.in_year(year).count
-  end
-
 end
--- spec/features/profile_spec.rb
+++ spec/features/profile_spec.rb
@@ -26,10 +26,6 @@ RSpec.describe "user profiles" do
     Given { activity(:wednesday, distance: 15_000, elevation: 100, seconds: 5_000, name: 'Neutral Wednesday 15k') }
 
     Then { expect(page).to have_text 'Streak 2 Nov. 6, 2017 to Nov. 13, 2017' }
-    And { expect(page).to have_text '21.7 miles' }
-    And { expect(page).to have_text '492 feet elev.' }
-    And { expect(page).to have_text '2.8 hours' }
-    And { expect(page).to have_text '3 activities' }
   end
 
 end
