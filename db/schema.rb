34e98940907d3e96b8f338bbee80bc6ceb67a529
Nick Holden <nick.r.holden@gmail.com>
2018-01-20 11:07:59 -0800

    Add deleted_at column to activities table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171216040942) do
+ActiveRecord::Schema.define(version: 20180120190657) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20171216040942) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "monday"
+    t.datetime "deleted_at"
     t.index ["user_id"], name: "index_activities_on_user_id"
   end
 
