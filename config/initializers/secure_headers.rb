e91706262a3ade2eb35b95ff8bb0efc0e064dbce
Nick Holden <nick.r.holden@gmail.com>
2018-03-01 20:35:57 -0800

    Add Google Analytics to CSP

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -2,7 +2,7 @@ SecureHeaders::Configuration.default do |config|
   config.csp = {
     preserve_schemes: true,
     default_src: %w('self'),
-    script_src: %w('self' 'unsafe-inline' 'unsafe-eval' www.googletagmanager.com use.fontawesome.com www.youtube.com s.ytimg.com),
+    script_src: %w('self' 'unsafe-inline' 'unsafe-eval' www.googletagmanager.com www.google-analytics.com use.fontawesome.com www.youtube.com s.ytimg.com),
     style_src: %w('self' 'unsafe-inline' fonts.googleapis.com use.fontawesome.com www.w3.org),
     img_src: %w('self' *.cloudfront.net maps.googleapis.com data:),
     font_src: %w('self' fonts.gstatic.com use.fontawesome.com),
