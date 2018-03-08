fb5b1bb060e0e0ea09c0049941e47e410aa84c4e
Nick Holden <nick.r.holden@gmail.com>
2018-03-07 21:28:03 -0800

    Support profile photos from Facebook

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -4,7 +4,7 @@ SecureHeaders::Configuration.default do |config|
     default_src: %w('self'),
     script_src: %w('self' 'unsafe-inline' 'unsafe-eval' www.googletagmanager.com www.google-analytics.com use.fontawesome.com www.youtube.com s.ytimg.com),
     style_src: %w('self' 'unsafe-inline' fonts.googleapis.com use.fontawesome.com www.w3.org),
-    img_src: %w('self' *.cloudfront.net www.google-analytics.com maps.googleapis.com data:),
+    img_src: %w('self' *.cloudfront.net www.google-analytics.com maps.googleapis.com graph.facebook.com data:),
     font_src: %w('self' fonts.gstatic.com use.fontawesome.com),
     media_src: %w('self' www.youtube.com),
     child_src: %w('self' www.youtube.com),
