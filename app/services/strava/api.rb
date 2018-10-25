ace3a4dc1795d78550c422426dac7f6d23dbcdbf
Nick Holden <nick.r.holden@gmail.com>
2018-10-24 18:15:00 -0700

    Add method for refreshing Strava access token

--- app/services/strava/api.rb
+++ app/services/strava/api.rb
@@ -15,5 +15,24 @@ module Strava
       end
     end
 
+    def self.refresh_access_token(refresh_token:)
+      response = Excon.post('https://www.strava.com/oauth/token'.freeze,
+        query: {
+          client_id: ENV.fetch('STRAVA_CLIENT_ID'),
+          client_secret: ENV.fetch('STRAVA_CLIENT_SECRET'),
+          grant_type: 'refresh_token'.freeze,
+          refresh_token: refresh_token,
+        }
+      )
+
+      parsed_response = JSON.parse(response.body)
+
+      if response.status != 200
+        raise AuthorizationError, parsed_response
+      else
+        AccessToken.new(parsed_response)
+      end
+    end
+
   end
 end
